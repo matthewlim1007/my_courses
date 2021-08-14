500112addd1f1ed12a99722d524cfaa255b3b177
MatthewLim <mlim@yondu.com>
2021-08-14 21:03:32 +0800

    course 16

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,6 +1,8 @@
 
 import { createAuth } from '@keystone-next/auth';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
@@ -34,7 +36,9 @@ export default withAuth(config ({
         url: databaseUrl,
     },
     lists: createSchema({
-        User
+        User,
+        Product,
+        ProductImage,
     }),
     ui: {
         isAccessAllowed: ({ session }) => {
