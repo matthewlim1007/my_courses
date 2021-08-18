0f7525a0d1bec2768344bad978ca4e5ba23ee1e2
MatthewLim <mlim@yondu.com>
2021-08-18 20:49:52 +0800

    Course 41

--- frontend/components/Reset.js
+++ frontend/components/Reset.js
@@ -5,26 +5,34 @@ import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
 import DisplayError from './ErrorMessage';
 
-
-const REQUEST_RESET_MUTATION = gql`
-  mutation REQUEST_RESET_MUTATION($email: String!) {
-    sendUserPasswordResetLink(email: $email) {
+const RESET_MUTATION = gql`
+  mutation RESET_MUTATION($email: String!, $token: String!, $password: String!) {
+    redeemUserPasswordResetToken(
+      email: $email
+      token: $token
+      password: $password
+    ) {
       message
       code
     }
   }
 `;
 
-export default function RequestReset() {
+export default function Reset({ token }) {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
     password: '',
-    token: '',
+    token,
   });
 
-  const [reset, { data, loading, error }] = useMutation(REQUEST_RESET_MUTATION, {
+  const [reset, { data, loading, error }] = useMutation(RESET_MUTATION, {
     variables: inputs,
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
+
+  const successfulError = data?.redeemUserPasswordResetToken?.code
+    ? data?.redeemUserPasswordResetToken
+    : undefined;
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -36,10 +44,10 @@ export default function RequestReset() {
   return (
     <Form method="POST" onSubmit={handleSubmit}>
       <h2>Request a Password Reset</h2>
-      <DisplayError error={error} />
+      <DisplayError error={error || successfulError} />
       <fieldset>
         {data?.redeemUserPasswordResetToken === null && (
-          <p>Success! You can Now sign In!</p>
+          <p>Success! You can now Sign in!</p>
         )}
         <label htmlFor="email">
           Email
@@ -49,6 +57,17 @@ export default function RequestReset() {
             placeholder="Your Email Address"
             autoComplete="email"
             value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Passowrd"
+            autoComplete="password"
+            value={inputs.password}
             onChange={handleChange}
           />
         </label>
